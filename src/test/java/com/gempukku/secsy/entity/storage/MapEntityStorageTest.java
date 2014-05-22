dfa988f84701a956ecc0ecd9c5f73bf95f7f8a65
MarcinSc <marcins78@gmail.com>
2014-05-22 17:35:28 +0100

    Additional testing.

--- src/test/java/com/gempukku/secsy/entity/storage/MapEntityStorageTest.java
+++ src/test/java/com/gempukku/secsy/entity/storage/MapEntityStorageTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 public class MapEntityStorageTest {
     private MapEntityStorage mapEntityStorage = new MapEntityStorage();
@@ -25,6 +26,16 @@ public class MapEntityStorageTest {
         final int id = mapEntityStorage.storeNewEntity(entity);
         assertSame(entity, mapEntityStorage.getEntityById(id));
         Mockito.verifyNoMoreInteractions(entity);
+    }
+
+    @Test
+    public void hasEntity() {
+        final EntityRef entity = Mockito.mock(EntityRef.class);
+        assertFalse(mapEntityStorage.hasEntity(entity));
+        final int id = mapEntityStorage.storeNewEntity(entity);
+        assertTrue(mapEntityStorage.hasEntity(entity));
+        mapEntityStorage.removeEntity(entity);
+        assertFalse(mapEntityStorage.hasEntity(entity));
     }
 
     @Test
